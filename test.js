85f6bce8c1d601ec40502be21796b88100736f5b
Kazumi Miura <bluesky-dev@aoisora.red>
2023-10-23 10:16:07 +0900

    変更前

---
+++ test.js
@@ -0,0 +1,18 @@
+// サムネイル画像要素の配列を取得
+const thumbnails = document.querySelectorAll(".gallery-thumbnails .thumbnail img");
+
+// gallery-mainクラス内のメイン画像要素を取得
+const mainImage = document.querySelector(".gallery-main img");
+
+// 各サムネイルにクリックイベントリスナを追加
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener("click", function() {
+        // クリックされたサムネイルの画像のsrc属性を取得
+        const clickedImageSrc = this.getAttribute("src");
+        const clickedImageAlt = this.getAttribute("alt");
+
+        // メイン画像のsrc属性とalt属性を更新
+        mainImage.setAttribute("src", clickedImageSrc);
+        mainImage.setAttribute("alt", clickedImageAlt);
+    });
+});
