677466f6740709397263ee0401a8d6be2bb08137
Kazumi Miura <bluesky-dev@aoisora.red>
2023-10-23 16:44:31 +0900

    kadai Accordion with JS done

---
+++ Accordion_js/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+{
+
+const dts = document.querySelectorAll('dt');
+
+dts.forEach(dt =>{
+  dt.addEventListener('click', () => {
+    dt.parentNode.classList.toggle('appear');
+  });
+});
+
+
+
+
+
+}
