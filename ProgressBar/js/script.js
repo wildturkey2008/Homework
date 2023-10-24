c5387a361765bb31c7d5c68e9cd3ab5604bf1670
Kazumi Miura <bluesky-dev@aoisora.red>
2023-10-24 11:40:32 +0900

    kadai ProgressBar done

--- ProgressBar/js/script.js
+++ ProgressBar/js/script.js
@@ -1,21 +1,49 @@
+'use strict';
+
 document.addEventListener("DOMContentLoaded", () => {
-  const startButton = document.getElementById("startButton");
   const progressBar = document.getElementById("progressBar");
+  const modal = document.getElementById("modal");
+  const mask = document.getElementById("mask");
+  const startButton = document.getElementById("startButton");
   const progressContainer = document.getElementById("progressContainer");
 
   startButton.addEventListener("click", () => {
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+
     // プログレスバーのコンテナを表示
     progressContainer.style.display = "block";
 
+    // クリックイベントを無効化
+    modal.style.pointerEvents = "none";
+    mask.style.pointerEvents = "none";
+    startButton.style.pointerEvents = "none";
+
     // プログレスバーの進行をシミュレート
     let width = 0;
+    const progress = document.getElementById('progress');
     const interval = setInterval(() => {
       if (width >= 100) {
         clearInterval(interval);
+        // クリックイベントを有効化
+        modal.style.pointerEvents = "auto";
+        mask.style.pointerEvents = "auto";
+        startButton.style.pointerEvents = "auto";
       } else {
         width++;
         progressBar.style.width = width + "%";
+        progress.textContent = `ダウンロード中 ${width}%`;        
       }
     }, 50); // 50ミリ秒ごとに更新
   });
+
+  modal.addEventListener("click", () => {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+
+  mask.addEventListener("click", () => {
+    modal.click();
+  });
+
 });
