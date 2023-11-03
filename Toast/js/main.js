7c8831a8c3f2bd9ea9f2db527bb4138e2046362d
Kazumi Miura <bluesky-dev@aoisora.red>
2023-11-03 11:39:51 +0900

    バグ修正のためスクロールの移動方向を検知するよう変更

--- Toast/js/main.js
+++ Toast/js/main.js
@@ -13,6 +13,7 @@
     let isTimeoutActive = false; // setTimeoutの動作状態を示すフラグ
     let isToastVisible = false; // トーストの表示状態を追跡するフラグ
     let hasToastBeenShown = false; // トーストが表示されたかを追跡するフラグ
+    let lastScrollTop = window.scrollY; // 最後のスクロール位置を保持する変数
     // 最下部から上にスクロールした時に再発火するのを防止
 
     // トーストを閉じる関数
@@ -27,21 +28,36 @@
     };
 
     window.addEventListener("scroll", function () {
-      // スクロールが一番下に達したかどうかを確認
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight)
-        if (!isToastVisible && !hasToastBeenShown) {
-          // トーストを表示
-          toast.classList.add("show");
-          isToastVisible = true;
-          hasToastBeenShown = true;
+      // ここで値をコンソールに出力
+      // console.log("window.innerHeight:", window.innerHeight);
+      // console.log("window.scrollY:", window.scrollY);
+      // console.log("addition:", window.innerHeight + window.scrollY);
+      // console.log("document.body.offsetHeight:", document.body.offsetHeight);
 
-          // 10秒後にトーストを自動で閉じる
-          timeoutId = setTimeout(closeToast, 10000);
-          // alert(timeoutId);
-          isTimeoutActive = true;
-        } else {
-          hasToastBeenShown = false;
-        }
+      let currentScrollTop = window.scrollY;
+
+      // 下方向にスクロールしていることを確認
+      if (currentScrollTop > lastScrollTop) {
+        // スクロールが一番下に達したかどうかを確認
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight)
+          if (!isToastVisible && !hasToastBeenShown) {
+            // トーストを表示
+            toast.classList.add("show");
+            isToastVisible = true;
+            hasToastBeenShown = true;
+            // 3秒後にトーストを自動で閉じる
+            timeoutId = setTimeout(closeToast, 3000);
+            // alert(timeoutId);
+            isTimeoutActive = true;
+          } else {
+            hasToastBeenShown = false;
+          }
+      } else {
+        // 上にスクロールした場合は、トーストが再度表示されるようにフラグをリセット
+        hasToastBeenShown = false;
+      }
+      // 現在のスクロール位置を更新
+      lastScrollTop = currentScrollTop;
     });
 
     // バツボタンでトーストを閉じる
