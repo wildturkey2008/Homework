c930780744ba060c58b593822e64ac2e525b3dfc
Kazumi Miura <bluesky-dev@aoisora.red>
2023-11-04 16:38:00 +0900

    JS版完成

--- Memo/js/main.js
+++ Memo/js/main.js
@@ -5,15 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const titleInput = document.getElementById("title");
   const contentInput = document.getElementById("content");
   const memoList = document.querySelector(".memos");
-  let currentSelectedMemoId = null;
+  const h2value = document.querySelector("h2");
+
+  let currentSelectedMemoId = null; //メモが選択された状態かどうかのフラグ
 
   // （関数）メモをlocalstorageから読み込んで表示
   const loadMemos = () => {
     memoList.innerHTML = "";
 
     const keys = Object.keys(localStorage);
-
-    // console.log(keys);
 
     // IDの降順にソート（新しいものからリストの上に来るように）
     const sortedKeys = keys.map(Number).sort((a, b) => b - a);
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
       li.textContent = memoData.title;
       li.dataset.id = key;
 
+      // 既存のメモがクリックされた時の処理
       li.addEventListener("click", () => {
         currentSelectedMemoId = li.dataset.id;
         const selectedData = JSON.parse(
@@ -31,6 +32,8 @@ document.addEventListener("DOMContentLoaded", () => {
         );
         titleInput.value = selectedData.title;
         contentInput.value = selectedData.content;
+        saveButton.textContent = "上書き";
+        h2value.textContent = "編集";
         showDeleteButton();
       });
 
@@ -44,34 +47,40 @@ document.addEventListener("DOMContentLoaded", () => {
   const saveMemo = () => {
     const title = titleInput.value;
     const content = contentInput.value;
+    // memoオブジェクト生成
+    const memo = { title, content };
 
-    if (title && content) {
-      // タイムスタンプからuidを生成
-      const id = Date.now().toString();
+    // currentSelectedMemoIDがnullなら新規作成、nullでなければ編集
+    if (currentSelectedMemoId === null) {
+      if (title && content) {
+        // タイムスタンプからuidを生成
+        const id = Date.now().toString();
 
-      // memoオブジェウト生成
-      const memo = { title, content };
+        // localstorageへ保存
+        localStorage.setItem(id, JSON.stringify(memo));
 
-      // console.log(`idは${id},コンテンツは${memo}`);
-      console.log(memo);
-
-      // localstorageへ保存
-      localStorage.setItem(id, JSON.stringify(memo));
-
-      // 入力フィールをクリアする
-      titleInput.value = "";
-      contentInput.value = "";
-
-      // 画面に反映
-      loadMemos();
+        // 入力フィールをクリアする
+        titleInput.value = "";
+        contentInput.value = "";
+      } else {
+        alert("タイトルの内容を入力して下さい");
+      }
     } else {
-      alert("タイトルの内容を入力して下さい");
+      // localstorageへ保存
+      if (confirm("データを上書きしますか？")) {
+        localStorage.setItem(currentSelectedMemoId, JSON.stringify(memo));
+      }
     }
+
+    // 画面に反映
+    loadMemos();
   };
 
   // （関数）新規作成ボタンで初期化
   const clearContents = () => {
     currentSelectedMemoId = null;
+    saveButton.textContent = "保存";
+    h2value.textContent = "新規作成";
     // 入力フィールをクリアする
     titleInput.value = "";
     contentInput.value = "";
@@ -80,9 +89,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // （関数）メモの削除
   const deleteMemo = () => {
-    if (currentSelectedMemoId != null) {
-      // console.log(currentSelectedMemoId);
-
+    if (confirm("メモを削除しますか？")) {
+      // 削除対象のli要素を取得
       const selectedLi = document.querySelector(
         `[data-id="${currentSelectedMemoId}"]`
       );
@@ -101,10 +109,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // （関数）削除ボタンの表示切り替え
   const showDeleteButton = () => {
     if (currentSelectedMemoId === null) {
-      console.log(currentSelectedMemoId);
       deleteButton.style.display = "none";
     } else {
-      console.log(currentSelectedMemoId);
       deleteButton.style.display = "block";
     }
   };
